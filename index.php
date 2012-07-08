061223ea53d1e5855ac3f334c950afeb6c93ccb3
unknown <asal@asal-PC.(none)>
2012-07-09 01:49:33 +0430

    index

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link type="text/css" rel="stylesheet" href="css/reset.css" />
+<link type="text/css" rel="stylesheet" href="css/menu.css" />
+<link type="text/css" rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+    <div class="container">
+    	<div class="header">
+        	<div class="left left-header">
+            	<img src="img/3.png" />
+            </div>
+            <div  class="rigth rigth-header">
+            	<div class="up-hed">
+                	<?php include("inc/menu.php"); ?>
+                </div>
+                <div class="down-hed">
+                	<img src="img/4.png" />
+                </div>
+            </div>
+            <div class="clear"></div>
+        </div>
+        <div class="content">
+        	<div class="slider">
+            	<div class="slider-init">
+                	<img src="img/5.png" />
+                </div>
+                <div class="div-opacity">Site Of The Month AWWWards</div>
+            </div>
+            <div class="text-pic">
+            	 <div class="left le">
+                 <img src="img/6.png" />
+                 	<div class="left-wirite"></div>
+                 </div>
+                 <div class="left le"><img src="img/7.png" /></div>
+                 <div class="le2"><img src="img/8.png" /></div>
+                 <div class="clear"></div>
+            </div>
+        </div>
+        <div class="footer">
+        	<div class="fot-up">
+            	 <div class="left fot1"><img src="img/9.png" /></div>
+                 <div class="left fot1"><img src="img/10.png" /></div>
+                 <div class="left fot1"><img src="img/11.png" /></div>
+                 <div class="fot2"><img src="img/12.png" /></div>
+                 <div class="clear"></div>
+            </div>
+            <div class="fot-down">
+            	 <div class="left fot-downleft"><img src="img/13.png" /></div>
+                 <div class="right fot-downright"><img src="img/15.png" /></div>
+                 <div class="fot-downcenter"><img src="img/14.png" /></div>
+                 <div class="clear"></div>
+            </div>
+            </div>
+       </div>
+    </div>
+</body>
+</html>
