be9bdfdf6c0f0ff2f0305192bedaca92163a6dc8
Ali mihandoost <ali@mihandoost.com>
2012-07-23 18:31:51 +0330

    deleting

--- README.md
+++
@@ -1,4 +0,0 @@
-test-repo
-=========
-
-its only a test
--- js/ani.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$('img.fade').hover(function(){
-		$(this).animate({'opacity':'0.5'},100,'');
-		
-	},function(){
-		$(this).animate({'opacity':1},400,'');
-	
-	});
-});
